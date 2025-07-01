76f2bef2ca27cd870e3249b878e4793079e0652b
Chinmayi Ranade <chinmayi.a.ranade@gmail.com>
2025-07-01 21:44:22 +0000

    Updated GenAI file

--- prompt.py
+++ prompt.py
@@ -2,6 +2,7 @@ import os
 from google import genai
 from google.genai import types
 from apiYelp import get_attractions
+from db import save_attractions
 
 # Set environment variables
 my_api_key = os.getenv('GENAI_KEY')
@@ -21,10 +22,16 @@ days = input("How many days are you traveling for? ")
 
 attractions = get_attractions(destination)
 
+
+
 print('\n Top attractions from Yelp: ')
 
 for i, name in enumerate(attractions, 1):
-  print(f'{i}. {name}')
+  print(f"{i}. {name['name']}")
+  print(f" 📍  Address: {name['address']}")
+  print(f" ⭐  Rating: {name['rating']}")
+  print(f" 🔗  More info: {name['url']}\n")
+  
 
 print('\n')
 
@@ -32,7 +39,7 @@ print('\n')
 names = []
 
 for name in attractions:
-  line = f"- {name}"
+  line = f"- {name} ({name['address']}, Rating: {name['rating']}"
   names.append(line)
 
 attractions_list = "\n".join(names)
@@ -40,10 +47,10 @@ attractions_list = "\n".join(names)
   
 prompt = (
   f"You are a travel assistant. Based on the following top places in {destination}:\n"
-    f"{attractions_list}\n\n"
-    f"Create a {days}-day travel itinerary that includes these attractions. "
-    f"Each day's plan should be 3–4 sentences long, combining activities and meals. "
-    f"Avoid overly long descriptions. Be friendly and informative, but keep it tight."
+  f"{attractions_list}\n\n"
+  f"Create a {days}-day travel itinerary that includes these attractions. "
+  f"Each day's plan should be 4-5 sentences long, combining activities and meals. "
+  f"Avoid overly long descriptions. Be friendly and informative, but keep it tight."
 )
   
 response = client.models.generate_content(
